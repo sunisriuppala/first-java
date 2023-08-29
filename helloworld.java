bccaf96a18baab7e1fbbc92f27e5ef21c527b4ec
Pavithra <sir@rats.com>
2023-08-29 18:18:53 +0000

    gsdsfsd

--- helloworld.java
+++ helloworld.java
@@ -4,6 +4,7 @@ class HelloWorld {
   System.out.println("Hello World");
   System.out.println("from vm new");
   System.out.println("I am handling manythings");
+  System.out.println("multiple");
   }
 
 }
