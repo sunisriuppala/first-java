22e1751c0adfd52897012dbaa6d5bd69e3b10eed
rudhraPraksah <rudra@onetop.com>
2023-08-29 17:04:03 +0000

    new one

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@
 class HelloWorld {
   public static void main(String[] args) {
   System.out.println("Hello World");
-  System.out.println("from vm");
+  System.out.println("from vm new");
   }
 
 }
